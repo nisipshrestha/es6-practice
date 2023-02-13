991bfc3e48287da1d42efdbc882c8bd6c3d2abde
Nisip Shresth <nisip.shrestha@swifttech.com.np>
2023-02-13 07:50:33 +0545

    nodejs specific import_export

--- Example3/node_js_specific_export.js
+++ Example3/node_js_specific_export.js
@@ -8,6 +8,8 @@ function addFn( param1, param2) {
 /* IMPORTANT NOTE: THERE SHOULD BE ONLY 1 DEFAULT EXPORT IN A FILE.
     AN INDIVIDUAL FILE IS TREATED AS AN INDIVIDUAL MODULE.
 */
+// Default export
+module.exports = addFn;
 
-module.exports = addFn;
-module.exports = addFn;
+// Named export
+module.exports.addFn = addFn;
--- Example3/node_js_specific_import.js
+++ Example3/node_js_specific_import.js
@@ -1,6 +1,10 @@
 
 // Example for default import.
-const addFn = require("./node_js_specific_export");
+const addFn1 = require("./node_js_specific_export");
 
 // console.log(addFn(1,2));
 
+const { addFn } = require('./node_js_specific_export');
+
+console.log("addFn",addFn(1,2));
+console.log("addFn1",addFn1(1,2));
